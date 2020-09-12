309194a82d7fddf886c455e147dd51cf328d428f
Amy Gao <amygao990@gmail.com>
2020-09-12 17:46:43 -0400

    initial commit

---
+++ src/components/my-navbar/mynavbar.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './mynavbar.style.css';
+
+const MyNavbar = () => { 
+    return (
+        <>
+        <Navbar collapseOnSelect fixed="top" expand="md" bg="dark" variant="dark" className="animate-navbar nav-theme justify-content-between">
+  <Navbar.Brand href="#home">
+
+    A.G
+  </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="ml-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link href="#skills">Skills</Nav.Link>
+      <Nav.Link href="#experience">Experience</Nav.Link>
+      <Nav.Link href="#projects">Projects</Nav.Link> 
+    </Nav>
+    
+  </Navbar.Collapse>
+</Navbar>
+        </>
+
+    )
+
+}
+export default MyNavbar
